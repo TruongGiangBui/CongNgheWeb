d487a3299679270776cbe83064ec34ca41672d57
TruongGiangBui <truonggiangbui9@gmail.com>
2021-04-28 20:24:44 +0700

    update

--- lab4/framework/config/config.php
+++ lab4/framework/config/config.php
@@ -5,12 +5,12 @@
 define ('DEVELOPMENT_ENVIRONMENT',true);
 
 
-define('DB_NAME', 'framework');
+define('DB_NAME', 'test_framework');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_HOST', 'localhost');
+define('DB_PASSWORD', 'Tuonggiang_08');
+define('DB_HOST', 'localhost:3306');
 
-define('BASE_PATH','http://localhost/framework');
+define('BASE_PATH','http://localhost/lab4/framework');
 
 
 define('PAGINATE_LIMIT', '5');
